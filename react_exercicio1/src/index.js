b0d5c2f237e403977c1df82280dd30af045b20e8
Victor Luiz Flor <victorflor56@gmail.com>
2023-02-15 22:15:08 -0400

    Jogo da Velha e Testando React

---
+++ react_exercicio1/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import './index.css'
+
+class Header extends React.Component{
+
+    render(){
+        return(
+        
+        <button>teste</button>
+        
+        )
+    }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Header/>);
